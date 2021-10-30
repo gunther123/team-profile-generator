90fc95b272aeab4a30a07b3a80579d36b07692e7
Steven Slocum <stevesl23@hotmail.com>
2021-10-30 13:03:55 -0500

    Created intern testing suite that fails currently

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,33 @@
 const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern')
+
+test('creates a new intern object', () =>{
+    //Create new intern
+    const intern = new Intern('Dave', 1, 'dave@gg.com', 'Greenway HighSchool');
+
+    //Check all inherited properties to confirm they are receiving the correct info from args
+    expect(intern.name).toBe('Dave');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toBe('dave@gg.com');
+
+    //Check intern property specific to intern object
+    expect(intern.school).toBe('Greenway HighSchool')
+});
+
+test('confirm role is overwritten on intern creation', () =>{
+    const employee = new Employee('Dave', 1, 'dave@gg.com', 'Greenway HighSchool');
+    //Confirm role is being set to Employee from the parent still
+    expect(employee.getRole()).toBe('Employee');
+
+    //Create new intern
+    const intern = new Intern('Steve', 2, 'steve@gg.com', 'Greenway HighSchool');
+    //Confirm that role gets overwritten to intern
+    expect(intern.getRole()).toBe('Intern');
+
+});
+
+test('confirm getOfficeNumber returns the School property', () => {
+    const intern = new Intern('Steve', 1, 'steve@gg.com', 'Greenway HighSchool');
+
+    expect(intern.getSchool()).toBe('Greenway HighSchool')
+})
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,40 +1,40 @@
 const Employee = require('../lib/Employee');
-const engineer = require('../lib/engineer');
+const Manager = require('../lib/Manager');
 
-test('creates a new Engineer object', () =>{
-    //Create new Engineer
-    const engineer = new Engineer('Dave', 1, 'dave@gg.com', 1);
+test('creates a new manager object', () =>{
+    //Create new manager
+    const manager = new Manager('Dave', 1, 'dave@gg.com', 1);
 
     //Check all inherited properties to confirm they are receiving the correct info from args
-    expect(engineer.name).toBe('Dave');
-    expect(engineer.id).toEqual(expect.any(Number));
-    expect(engineer.email).toBe('dave@gg.com');
+    expect(manager.name).toBe('Dave');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toBe('dave@gg.com');
 });
 
-test('confirm role is overwritten on engineer creation', () =>{
+test('confirm role is overwritten on Manager creation', () =>{
     const employee = new Employee('Dave', 1, 'dave@gg.com', 1);
     //Confirm role is being set to Employee from the parent still
     expect(employee.getRole()).toBe('Employee');
 
-    //Create new Engineer
-    const engineer = new Engineer('Steve', 2, 'steve@gg.com', 1);
-    //Confirm that role gets overwritten to engineer
-    expect(engineer.getRole()).toBe('engineer');
+    //Create new manager
+    const manager = new Manager('Steve', 2, 'steve@gg.com', 1);
+    //Confirm that role gets overwritten to Manager
+    expect(manager.getRole()).toBe('Manager');
 
 });
 
 test('confirm office number property is being created', () =>{
-    const engineer = new Engineer('Steve', 1, 'steve@gg.com', 1);
+    const manager = new Manager('Steve', 1, 'steve@gg.com', 1);
     //check that assigned arg for officeNumber assigns correctly
-    expect(engineer.officeNumber).toEqual(1);
+    expect(manager.officeNumber).toEqual(1);
 
-    const engineerTwo = new Engineer('bob', 4, 'bob@gg.com',2);
+    const managerTwo = new Manager('bob', 4, 'bob@gg.com',2);
 
-    expect(engineerTwo.officeNumber).toEqual(2);
+    expect(managerTwo.officeNumber).toEqual(2);
 });
 
 test('confirm getOfficeNumber returns the officeNumber property', () => {
-    const engineer = new Engineer('Steve', 1, 'steve@gg.com', 5544);
+    const manager = new Manager('Steve', 1, 'steve@gg.com', 5544);
 
-    expect(engineer.getOfficeNumber()).toEqual(5544)
+    expect(manager.getOfficeNumber()).toEqual(5544)
 })
