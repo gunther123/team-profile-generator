f6c611dd94f422e2add115a00db8a3fc25c85973
Steven Slocum <stevesl23@hotmail.com>
2021-10-29 20:10:39 -0500

    Created test for getID function

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,11 +7,18 @@ test('creates a new employee object', () => {
     expect(employee.name).toBe('Dave');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe('steve@yahoo.com')
-})
+});
 
 //Test Employee getName function
 test("get employee's name from the Employee object", () => {
     const employee = new Employee('Dave', 1, 'steve@yahoo.com');
 
     expect(employee.getName()).toHaveProperty('name');
-})
+});
+
+//Test Employee getID function
+test("get employee ID from the Employee object", () => {
+    const employee = new Employee('Dave', 1, 'steve@yahoo.com');
+
+    expect(employee.getID()).toEqual(expect.any(Number));
+});
