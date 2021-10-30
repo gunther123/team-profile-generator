266f81b6931781ec9d26208ac5e901a7e78f60b2
Steven Slocum <stevesl23@hotmail.com>
2021-10-29 19:47:19 -0500

    Created all object key pair values for the Employee Class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,10 @@
 const Employee = require('../lib/Employee')
 
+//Tests employee Object creation
 test('creates a new employee object', () => {
-    const employee = new Employee('Dave');
+    const employee = new Employee('Dave', 1, 'steve@yahoo.com');
 
     expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('steve@yahoo.com')
 })
