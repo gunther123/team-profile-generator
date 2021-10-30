6b58eef60430e277d82a818c56f97b5f140f8749
Steven Slocum <stevesl23@hotmail.com>
2021-10-29 19:53:04 -0500

    Created second test for getName function

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,17 @@
 const Employee = require('../lib/Employee')
 
-//Tests employee Object creation
+//Tests Employee Object creation
 test('creates a new employee object', () => {
     const employee = new Employee('Dave', 1, 'steve@yahoo.com');
 
     expect(employee.name).toBe('Dave');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe('steve@yahoo.com')
+})
+
+//Test Employee getName function
+test("get employee's name from the Employee object", () => {
+    const employee = new Employee('Dave', 1, 'steve@yahoo.com');
+
+    expect(employee.getName().toHaveProperty('name'));
 })
